488f7eb149bb7d7ddec381d4511beff91f91e809
Rover van de Ven <s098795@ap.be>
2019-09-04 14:29:56 +0200

    import json and create html

--- overtreding/overtredingen/views.py
+++ overtreding/overtredingen/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import json
+
+def index(request, aantal):
+    with open('infractions.json') as data_file:
+        infractionsjson = json.load(data_file)
+
+    overtredingen = []
+    for x in infractionsjson:
+        if x["infractions_speed"] >= aantal:
+            overtredingen.append(x)
+
+    overtredingen.sort(key=lambda x: x.infractions_speed)
+       
+    context = {
+        "overtredingenlijst": overtredingen
+    }
+
+    return render(request, "index.html", context)
+
 
 # Create your views here.
