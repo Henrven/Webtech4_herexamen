3b74a90be4679f638780996c9c3316c55cd26f72
Rover van de Ven <s098795@ap.be>
2019-09-04 14:49:10 +0200

    add paramater to path

--- overtreding/overtredingen/views.py
+++ overtreding/overtredingen/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 import json
 
-def index(request, aantal):
+def index(request, value):
     with open('infractions.json') as data_file:
         infractionsjson = json.load(data_file)
 
     overtredingen = []
     for x in infractionsjson:
-        if x["infractions_speed"] >= aantal:
+        if x['infractions_speed'] >= int(value):
             overtredingen.append(x)
 
-    overtredingen.sort(key=lambda x: x.infractions_speed)
-       
+    overtredingen.sort(key=lambda x: x['infractions_speed'])
+    print (overtredingen);
+
     context = {
         "overtredingenlijst": overtredingen
     }
